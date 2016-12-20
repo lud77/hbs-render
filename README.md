f8c5f90620eb6e9f506902e94eceafa5eb782e74
Stefano Butera <Stefano Butera stefano.butera@nethnic.com>
2016-12-20 18:43:18 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+![Nethnic Logo](pkg/logo/logo.jpg)
+
+# v1.0.0
+> A CLI tool to render Handlebars templates with partials
+
+
+## Installation
+
+Clone the repository
+
+	npm i hbsrender --save
+
+
+## Why?
+
+The standard Handlebars CLI interface doesn't allow you to include sub-templates (template parts) in templates to be rendered on the server.
+
+
+# Features
+
+This module has a single purpose: to provide a CLI interface to render Handlebars templates server-side. It does that by giving you parameters to specify paths to template parts and to a JSON file containing the context.
+
+Note: the output of this tool is not a compiled Handlebars template. The template is compiled in-memory and immediately used with the context data to generate a render.
+
+
+## Usage
+
+./node_modules/.bin/hbsrender [options]
+
+Options:
+
+	-h, --help                 output usage information
+	-V, --version              output the version number
+	-t, --template <template>  tempate to render
+	-p, --partial <partial>    partial to make available to the template
+	-c, --context <context>    JSON file with data to be made available to the template
+
+You can use any number of "-p" parameters to specify as many template parts as you need.
+The value of the "-p" parameter <partial> must be specified in the format *partial-name:path-to-file*.
+
+
+## Example
+
+	./node_modules/.bin/hbsrender -t main_template.hbs -c ./context.json -p part1:parts/part1.hbs -p part2:parts/part2.hbs
+
+
+This will render the main_template.hbs to standard output, pulling variables for the rendering from context.json.
+
+To render to a file you can do:
+
+	./node_modules/.bin/hbsrender [options] > output.html
+
+
+In this example, inside main_template.hbs you can refer to the partials "part1" and "part2" respectively using {{#> part1}} and {{#> part2}}.
+
+You can also provide default replacementes for a partial (should it be not provided on the command line), with the following snippet:
+
+	{{#> part1}}
+		
+		...fallback content here...
+	
+	{{/part1}}
+
+
+## Licensing
+
+This package is released under the [MIT License](https://opensource.org/licenses/MIT)
+
---
+++ bin/hbsrender.js
@@ -0,0 +1,86 @@
+#!/usr/bin/env node
+
+"use strict";
+
+const options = require("commander");
+const hbs = require("handlebars");
+const fs = require("fs");
+const packageJson = require('../package.json');
+
+/**
+ * Populate the list of partials passed with the -p CLI option
+ */
+function collectPartials(val, memo) {
+	let parts = val.split(':');
+	if (parts.length === 2) {
+		memo.names.push(parts[0]);
+		memo.files.push(parts[1]);
+	} else {
+		console.log('invalid format for parameter "-p ' + val + '" please specify [partial_name]:[path/to/file.hbs]');
+	}
+}
+
+/**
+ * Load the partials if they exist
+ */
+function loadPartials(partials, callback, ndx) {
+	let i = (ndx === undefined) ? 0 : ndx;
+	if (partials.files[i] !== undefined) {
+		fs.readFile(partials.files[i], function(err, buffer) {
+			if (err) {
+				partials.contents.push(false);
+			} else {
+				partials.contents.push(buffer.toString('utf8'));
+			}
+
+			loadPartials(partials, callback, i + 1);
+		});
+	} else {
+		setImmediate(callback);
+	}
+}
+
+/**
+ * Register the partials and render the template in the specified context
+ */		 
+function render(template, partials, context) {
+	let len = partials.names.length;
+	for (let n = 0; n < len; n++) {
+		if (partials.contents[n] !== false) {
+			hbs.registerPartial(partials.names[n], partials.contents[n]);
+		}
+	}
+
+	var res = hbs.compile(template)(context);
+	console.log(res);
+}
+
+let partials = {
+	names: [],
+	files: [],
+	contents: []
+};
+
+options
+	.version(packageJson.version)
+	.option('-t, --template <template>', 'tempate to render')
+	.option('-p, --partial <partial>', 'partial to make available to the template', collectPartials, partials)
+	.option('-c, --context <context>', 'JSON file with data to be made available to the template')
+	.parse(process.argv);
+
+// Load the template file, the context file and the partials, then launch rendering
+if (options.template !== undefined) {
+	fs.readFile(options.template, function(err, buffer) {
+		let template = buffer.toString('utf8');
+		loadPartials(partials, function() {
+			if (options.context !== undefined) {
+				fs.readFile(options.context, function(err, buffer) {
+					let context = JSON.parse(buffer.toString('utf8'));
+					render(template, partials, context);
+				});
+			} else {
+				render(template, partials, {});
+			}
+		});
+	});
+}
