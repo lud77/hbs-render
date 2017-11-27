f31cfe1cd90ebb53462715c9bc2bcc16cc90b6be
Stefano Butera <stefano.butera@nethnic.com>
2017-11-27 07:10:16 +0000

    Switch to arrow functions

--- bin/hbsrender.js
+++ bin/hbsrender.js
@@ -10,7 +10,7 @@ const packageJson = require('../package.json');
 /**
  * Populate the list of partials passed with the -p CLI option
  */
-function collectPartials(val, memo) {
+const collectPartials = (val, memo) => {
 	let parts = val.split(':');
 	if (parts.length === 2) {
 		memo.names.push(parts[0]);
@@ -18,12 +18,12 @@ function collectPartials(val, memo) {
 	} else {
 		console.log('invalid format for parameter "-p ' + val + '" please specify [partial_name]:[path/to/file.hbs]');
 	}
-}
+};
 
 /**
  * Load the partials if they exist
  */
-function loadPartials(partials, callback, ndx) {
+const loadPartials = (partials, callback, ndx) => {
 	let i = ndx || 0;
 	if (partials.files[i] !== undefined) {
 		fs.readFile(partials.files[i], function(err, buffer) {
@@ -38,19 +38,19 @@ function loadPartials(partials, callback, ndx) {
 	} else {
 		setImmediate(callback);
 	}
-}
+};
 
 /**
  * Register the partials and render the template in the specified context
  */		 
-function render(template, partials, context) {
+const render = (template, partials, context) => {
 	partials.contents.map((content, n) => {
 		if (content && partials.names[n]) hbs.registerPartial(partials.names[n], content);
 	});
 
 	var res = hbs.compile(template)(context);
 	console.log(res);
-}
+};
 
 let partials = {
 	names: [],
